b6473d26a8dd966db746261440fb4825ab64d4e2
Laura Cemi Iora <1131015@atitus.edu.br>
2023-11-20 21:43:54 -0300

    Colocando Imagens

--- start.py
+++ start.py
@@ -1,2 +1,15 @@
 import pygame
 pygame.init
+tamanho = (509,360)
+branco = (255,255,255)
+preto = (0,0,0)
+clock = pygame.time.Clock()
+tela = pygame.display.set_mode(tamanho)
+fundo = pygame.image.load('fundo.jpg')
+pygame.display.set_caption('Space Discovery')
+rodando = True
+while rodando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
+    tela.blit(fundo, (0,0))
